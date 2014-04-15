49166d08264febf95044e4eb2279d5964b2c3785
yaozhen <pop59461@126.com>
2014-04-15 20:40:39 +0800

    captcha
    Signed-off-by: yaozhen <pop59461@126.com>

---
+++ application/controllers/test.php
@@ -0,0 +1,35 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Test extends CI_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->helper('url');
+		$this->load->model('console_imf');
+		$this->load->helper('captcha');
+	}
+
+	public function aaa()
+	{
+		$ranNum = rand(1000,9999);
+		$vals = array(
+		'word' => $ranNum,
+		'img_path' => './captcha/',
+		'img_url' => 'http://localhost/xunwu/captcha/',
+		'font_path' => './path/to/fonts/texb.ttf',
+		'img_width' => '100',
+		'img_height' => '30',
+		'expiration' => 7200
+		);
+
+		$date = create_captcha($vals);
+
+		$this->load->view('header');
+		$this->load->view('test',$date);
+		$this->load->view('footer');
+	}
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
