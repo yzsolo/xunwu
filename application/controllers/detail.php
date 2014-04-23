caf3c8b817d36b2e4385bfd3180c23822f0a9b8d
yaozhen <pop59461@126.com>
2014-04-23 17:36:42 +0800

    Signed-off-by: yaozhen <pop59461@126.com>

--- application/controllers/detail.php
+++ application/controllers/detail.php
@@ -5,13 +5,13 @@ class Detail extends CI_Controller {
 	public function __construct()
 	{
 		parent::__construct();
+		session_start();
 		$this->load->helper('url');
 		$this->load->model('console_imf');
 		$this->load->helper('captcha');
-		session_start();
 	}
 
-	private function create()
+	private function create()	// 标准方法创建验证码，并将验证码的值存到session
 	{
 		$img_url = base_url()."captcha/";
 		$ranNum = rand(1000,9999);
@@ -31,9 +31,9 @@ class Detail extends CI_Controller {
 	{
 		$id = $this->uri->segment(3);
 		$data['one_new'] = $this->console_imf->get_id_fmsg($id);
-		$data['image'] = $this->create();
-		$data['id'] = $id;
-		$data['table'] = "fmsg";
+		$data['image'] = $this->create();	// 验证码
+		$data['id'] = $id;	// 文章id，方便提交获取验证码
+		$data['table'] = "fmsg";	// 哪个数据库
 		// print_r($data);
 		$this->load->view('header');
 		$this->load->view('detail_f',$data);
@@ -53,7 +53,7 @@ class Detail extends CI_Controller {
 		$this->load->view('footer');
 	}
 
-	public function refresh()
+	public function refresh()	// 刷新验证码
 	{
 		$img_url = base_url()."captcha/";
 		$ranNum = rand(1000,9999);
@@ -70,14 +70,14 @@ class Detail extends CI_Controller {
 		echo $img_url.$date['time'].".jpg";
 	}
 
-	public function verify()
+	public function verify()	// 接收验证码的值，并判断是否正确，并且返回结果（联系方式）
 	{
 		$verify = $_POST['verify-input'];
 		$table = $_POST['verify-table'];
 		$id = $_POST['verify-id'];
 		$verify = sha1($verify.sha1("xunwu2014"));
 		if (isset($_SESSION['yanzhen'])) {
-			if ($verify == $_SESSION['yanzhen']) {
+			if ($verify === $_SESSION['yanzhen']) {
 				if ($table == 'lmsg') {
 					$data = $this->console_imf->get_id_lmsg($id);
 					$phone = $data[0]['l_telnum'];
