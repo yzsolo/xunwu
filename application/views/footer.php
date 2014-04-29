4840f53395365fbbe163b060ea957ea45c40cbc8
yaozhen <pop59461@126.com>
2014-04-29 20:42:08 +0800

    add link
    Signed-off-by: yaozhen <pop59461@126.com>

--- application/views/footer.php
+++ application/views/footer.php
@@ -2,13 +2,14 @@
 	<div id="relation_link">
 		<ul>
 			<li>相关链接</li>
-			<li><a href="http://www.stuzone.com">学生地带</a></li>
-			<li><a href="http://www.bitworkshop.net/">比特工场</a></li>
-			<li><a href="http://www1.scuec.edu.cn/stu/">学工在线</a></li>
-			<li><a href="http://guwen.stuzone.com/">故问</a></li>
-			<li><a href="http://upan.us/">云U盘</a></li>
-			<li><a href="http://www.stuzone.com/zone_video/">月芽网</a></li>
-			<li><a href="http://market.stuzone.com/">学生市场</a></li>
+			<li><a target="_blank" href="http://www.stuzone.com">学生地带</a></li>
+			<li><a target="_blank" href="http://www.bitworkshop.net/">比特工场</a></li>
+			<li><a target="_blank" href="http://www1.scuec.edu.cn/stu/">学工在线</a></li>
+			<li><a target="_blank" href="http://guwen.stuzone.com/">故问</a></li>
+			<li><a target="_blank" href="http://upan.us/">云U盘</a></li>
+			<li><a target="_blank" href="http://www.stuzone.com/zone_video/">月芽网</a></li>
+			<li><a target="_blank" href="http://market.stuzone.com/">学生市场</a></li>
+			<li><a target="_blank" href="http://www.stuzone.com/zixunminda/blog/">资讯民大</a></li>
 		</ul>
 	</div>
 	<ul name="one">
@@ -17,7 +18,7 @@
 		<li>联系我们</li>
 		<li>免责声明</li>
 	</ul>
-	<p>&copy;2011-2013&nbsp;stuzone.com&nbsp;版权所有&nbsp;鄂ICP备20138号&nbsp;Powered&nbsp;By&nbsp;比特工场<img src="<?php echo base_url('/resource/img/bit_logo_img.png') ?>" alt=""></p>
+	<p>&copy;2011-<?php echo date('Y'); ?>&nbsp;stuzone.com&nbsp;版权所有&nbsp;鄂ICP备20138号&nbsp;Powered&nbsp;By&nbsp;比特工场<img src="<?php echo base_url('/resource/img/bit_logo_img.png') ?>" alt=""></p>
 	 <script src="<?php echo base_url('/resource/js/jquery.js') ?>"></script> 
 	<!-- 为了兼容使用1.xx的版本 -->
 	<script src="http://cdn.staticfile.org/jquery/1.11.0/jquery.min.js"></script>
