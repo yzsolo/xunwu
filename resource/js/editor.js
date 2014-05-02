2bcbcab8edc752490c6736da2d46d2c6e6d46f30
yaozhen <pop59461@126.com>
2014-05-02 22:18:41 +0800

    固定编辑框大小
    Signed-off-by: yaozhen <pop59461@126.com>

--- resource/js/editor.js
+++ resource/js/editor.js
@@ -3,6 +3,7 @@ KindEditor.ready(function(K) {
 		themeType: 'simple',
 		height: '200px',
 		width: '420px',
+		resizeType: 0,
 		filterMode: true,
 		items: ['fontsize', 'forecolor', 'hilitecolor', 'bold', 'italic', 'underline', 'strikethrough',
 				'link', 'justifyleft', 'justifycenter', 'justifyright', 'insertorderedlist',
