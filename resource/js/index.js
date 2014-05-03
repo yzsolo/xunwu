da857dfe091f64c823713d74d6a06feff3e162e4
Aresy.z <yzsolo125@gmail.com>
2014-05-03 23:24:41 +0800

    js 重构

--- resource/js/index.js
+++ resource/js/index.js
@@ -1,11 +1,63 @@
 $(document).ready(function(){
  
- 	$('input[name=search_input]').focus(function(){
- 		$(this).keypress(function(){
- 			if(event.keyCode == 13){
- 				// $('.search_btn').on('click',function(){
+	var event_bind = {
+
+		eve_describe: {
+			'input[name=search_input] focus': 'focus',
+			'.search_btn click': 'search_btn',
+			'.ind_tab_lost mousedown': 'ind_tab_lost',
+			'.ind_tab_find mousedown': 'ind_tab_find',
+			'input[name=name_f] blur': 'check_name_f',
+			'input[name=locale_f] blur': 'check_local_f',
+			'input[name=finder_f] blur': 'check_finder_f',
+			'select[name=kind_f] blur': 'check_kind_f',
+			'input[name=telnum_f] blur': 'check_telnum_f',
+			'.things_submit_l click': 'things_submit_l',
+			'.things_submit_f click': 'things_submit_f',
+			'.fin_tab_select change': 'fin_tab_select_change',
+			'.los_tab_select change': 'los_tab-select_change'
+		},
+
+		eve_bind: function(){
+			for (var key in this.eve_describe) {
+
+				if(this.eve_describe.hasOwnProperty(key)){
+					var callback = this.eve_describe[key],
+					ele_eve = key.split(' '),
+					ele = ele_eve[0],
+					eve = ele_eve[1];
+
+					$(ele).on(eve,this.eve_detail[callback]);
+				}
+			}
+		},
+
+		eve_detail: {
+
+			focus : function() {
+				$(this).keypress(function(){
+		 			if(event.keyCode == 13){
+						var search_input = $('input[name="search_input"]').val();
+						$.ajax({
+							type:"post",
+							url:getRootPath()+"/index.php/defaults/search_result",
+							data:{
+								search_input:search_input
+							},
+							success:function(msg){
+								$('#nav_box').next().html(msg);
+								loadjs(getRootPath()+'/resource/js/hah.js');
+								ul_style();
+								page_effect();
+						        
+							}
+						})
+		 			}
+		 			$('#nav_box a').removeClass('nav_cur');
+		 		})
+			},
+			search_btn : function() {
 				var search_input = $('input[name="search_input"]').val();
-				console.log(search_input);
 				$.ajax({
 					type:"post",
 					url:getRootPath()+"/index.php/defaults/search_result",
@@ -13,196 +65,181 @@ $(document).ready(function(){
 						search_input:search_input
 					},
 					success:function(msg){
-						console.log("success");
 						$('#nav_box').next().html(msg);
 						loadjs(getRootPath()+'/resource/js/hah.js');
 						ul_style();
 						page_effect();
-				        
 					}
 				})
- 			}
- 			$('#nav_box a').removeClass('nav_cur');
- 		})
- 	})
-	
- 	$('.search_btn').on('click',function(){
-		var search_input = $('input[name="search_input"]').val();
-		console.log(search_input);
-		$.ajax({
-			type:"post",
-			url:getRootPath()+"/index.php/defaults/search_result",
-			data:{
-				search_input:search_input
+				$('#nav_box a').removeClass('nav_cur');
 			},
-			success:function(msg){
-				console.log("success");
-				$('#nav_box').next().html(msg);
-				loadjs(getRootPath()+'/resource/js/hah.js');
-				ul_style();
-				page_effect();
+			ind_tab_lost : function() {
+				$('.ind_tab_lost_con').css("display",'block');
+				$('.ind_tab_find_con').css("display",'none');
+				$(this).css({
+					"background-color":'#eee',
+					"background-image":'url(resource/img/tab_img_down.png)'
+				});
+				$(".ind_tab_find").css({
+					"background-color":"#fff",
+					"background-image":'url(resource/img/tab_img_up.png)'
+				});
+			},
+			ind_tab_find : function() {
+				$('.ind_tab_lost_con').css("display",'none');
+				$('.ind_tab_find_con').css("display",'block');
+				$(this).css({
+					"background-color":'#eee',
+					"background-image":"url(resource/img/tab_img_down.png)"
+				   
+				});
+				$(".ind_tab_lost").css({
+					"background-color":"#fff",
+					"background-image":'url(resource/img/tab_img_up.png)'
+				});
+			},
+			check_name_f : function() {
+				flag_name = this.value==""?false:true,str = "input[name=name_f]+div";
+				flag_style(flag_name,str);
+			},
+
+			check_local_f : function() {
+				flag_locale = this.value==""?false:true,str = "input[name=locale_f]+div";
+				flag_style(flag_locale,str);
+			},
+
+			check_finder_f : function() {
+				flag_finder = this.value==""?false:true,str = "input[name=finder_f]+div";
+				flag_style(flag_finder,str);
+			},
+
+			check_kind_f : function() {
+				flag_kind = this.value=="选择类型"?false:true, str = "select[name=kind_f]+div";
+				flag_style(flag_kind,str);
+			},
+
+			check_telnum_f : function() {
+				var reg = new RegExp("^1[3|5|8]{1}[0-9]{9}$");
+				flag_telnum = reg.test(this.value), str = "input[name=telnum_f]+div";
+				flag_style(flag_telnum,str);
+			},
+
+			things_submit_l : function() {
+				editor.sync();
+				var kind_f = $(".kind select[name=kind_f]").val(),
+					name_f = $("input[name=name_f]").val(),
+					locale_f = $("input[name=locale_f]").val(),
+					finder_f = $("input[name=finder_f]").val(),
+					telnum_f = $("input[name=telnum_f]").val(),
+					descri_f = $("textarea[name=descri_f]").val();
+				var flag = flag_kind&flag_name&flag_locale&flag_finder&flag_telnum;
+				if(flag){
+					$.ajax({
+						type:"POST",
+						url:getRootPath()+"/index.php/descri_lost/imformation",
+						data:{
+							kind_f:kind_f,
+							name_f:name_f,
+							locale_f:locale_f,
+							finder_f:finder_f,
+							telnum_f:telnum_f,
+							descri_f:descri_f
+						},
+						success:function(msg) {
+							alert("提交成功");
+							window.location.href=getRootPath()+"/index.php";
+					    }
+					})
+				}
+			},
+
+			things_submit_f : function() {
+				editor.sync();
+				var kind_f = $(".kind select[name=kind_f]").val(),
+					name_f = $("input[name=name_f]").val(),
+					locale_f = $("input[name=locale_f]").val(),
+					finder_f = $("input[name=finder_f]").val(),
+					telnum_f = $("input[name=telnum_f]").val(),
+					descri_f = $("textarea[name=descri_f]").val();
+				var flag = flag_kind&flag_name&flag_locale&flag_finder&flag_telnum;
+				if(flag){
+					$.ajax({
+					type:"POST",
+					url:getRootPath()+"/index.php/descri_find/imformation",
+					data:{
+						kind_f:kind_f,
+						name_f:name_f,
+						locale_f:locale_f,
+						finder_f:finder_f,
+						telnum_f:telnum_f,
+						descri_f:descri_f
+					},
+					success:function(msg) {
+						alert("提交成功");
+						window.location.href=getRootPath()+"/index.php";
+				    }
+					})
+				}
+			},
+
+			fin_tab_select_change : function() {
+				var kind = $(this).val();
+				var flag = 1;
+				$.ajax({
+					type:"POST",
+					url:getRootPath()+"/index.php/find/page_find_kind",
+					data:{
+						fkind:kind,
+						flag:flag
+					},
+					success:function(msg) {
+					   $(".fin_detail").html(msg);
+					   $('.fin_detail>ul:even li').css('background',"#ddd");
+					   ul_style();
+					   page_effect();
+				    }
+				})
+			},
+
+			los_tab_select_change : function() {
+				var kind = $(this).val();
+				var flag = 1;
+				$.ajax({
+					type:"POST",
+					url:getRootPath()+"/index.php/lost/page_lost_kind",
+					data:{
+						lkind:kind,
+						flag:flag
+					},
+					success:function(msg) {
+					   $(".los_detail").html(msg);
+					   $('.los_detail>ul:even li').css('background',"#ddd");
+					   ul_style();
+					   page_effect();
+				    }
+				})
 			}
-		})
-		$('#nav_box a').removeClass('nav_cur');
-	});
+		}
+
+	}
+
+	event_bind.eve_bind();
 
 
-
-	//test
-	$('.send_msg').on('click',function(){
-		var text_area = $('.write_msg textarea').val(),
-		    response_name = $('.write_msg input[name="your_name"]').val(),
-		    contact_imf = $('.write_msg input[name="contact_imf"]').val();
-		console.log(text_area);
-		$('<div class="response_div"><p class="response"><span class="response_span">'+response_name+" : </span><span>"+text_area+'</span></p></div>').insertBefore('.write_msg');
-		return false;
-	})
-
-
-	$('.ind_tab_lost').mousedown(function(){
-		$('.ind_tab_lost_con').css("display",'block');
-		$('.ind_tab_find_con').css("display",'none');
-		$(this).css({
-			"background-color":'#eee',
-			"background-image":'url(resource/img/tab_img_down.png)'
-		});
-		$(".ind_tab_find").css({
-			"background-color":"#fff",
-			"background-image":'url(resource/img/tab_img_up.png)'
-		});
-	});
 
 	$(".i_find_btn,.i_respond,.send_msg,.i_lost,.i_find,.descri_fin_con .things_submit_f,.descri_fin_con .things_submit_l").mouseenter(function(){
 		$(this).css("background-color","#e3170d");
-	}).mouseleave(function(){
+	}).mouseleave(function() {
 		$(this).css("background-color","#ff6766");
 	})
 
-	$('.ind_tab_find').mousedown(function(){
-		$('.ind_tab_lost_con').css("display",'none');
-		$('.ind_tab_find_con').css("display",'block');
-		$(this).css({
-			"background-color":'#eee',
-			"background-image":"url(resource/img/tab_img_down.png)"
-		   
-		});
-		$(".ind_tab_lost").css({
-			"background-color":"#fff",
-			"background-image":'url(resource/img/tab_img_up.png)'
-		});
-	});
-
-
-	/*表单验证 start*/
-	$(".things_style input[name=name_f]").blur(function(){
-		flag_name = this.value==""?false:true,str = "input[name=name_f]+div";
-		flag_style(flag_name,str);
-	})
-
-	$(".things_style input[name=locale_f]").blur(function(){
-		flag_locale = this.value==""?false:true,str = "input[name=locale_f]+div";
-		flag_style(flag_locale,str);
-	})
-
-	$(".things_style input[name=finder_f]").blur(function(){
-		flag_finder = this.value==""?false:true,str = "input[name=finder_f]+div";
-		flag_style(flag_finder,str);
-	})
-
-	$(".kind select[name=kind_f]").blur(function(){
-		flag_kind = this.value=="选择类型"?false:true, str = "select[name=kind_f]+div";
-		flag_style(flag_kind,str);
-	})
-
-	$(".things_style input[name=telnum_f]").blur(function(){
-		var reg = new RegExp("^1[3|5|8]{1}[0-9]{9}$");
-		 flag_telnum = reg.test(this.value), str = "input[name=telnum_f]+div";
-		flag_style(flag_telnum,str);
-	})
-
-	// $(".things_style input[name=email_f]").blur(function(){
-	// 	var reg = new RegExp("^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+((\.[a-zA-Z0-9_-]{2,3}){1,2})$");
-	// 	 flag_email = reg.test(this.value), str = "input[name=email_f]+div";
-	// 	flag_style(flag_email,str);
-	// })
-
-	// $(".things_style input[name=qq_f]").blur(function(){
-	// 	var reg = new RegExp("^[1-9]{1}[0-9]{4,10}$");
-	// 	 flag_qq = reg.test(this.value), str = "input[name=qq_f]+div";
-	// 	flag_style(flag_qq,str);
-	// })
-
-	$('.things_submit_l').click(function(){
-		editor.sync();
-		var kind_f = $(".kind select[name=kind_f]").val(),
-			name_f = $("input[name=name_f]").val(),
-			locale_f = $("input[name=locale_f]").val(),
-			finder_f = $("input[name=finder_f]").val(),
-			telnum_f = $("input[name=telnum_f]").val(),
-			// email_f = $("input[name=email_f]").val(),
-			// qq_f = $("input[name=qq_f]").val(),
-			descri_f = $("textarea[name=descri_f]").val();
-		var flag = flag_kind&flag_name&flag_locale&flag_finder&flag_telnum;
-		if(flag){
-			$.ajax({
-				type:"POST",
-				url:getRootPath()+"/index.php/descri_lost/imformation",
-				data:{
-					kind_f:kind_f,
-					name_f:name_f,
-					locale_f:locale_f,
-					finder_f:finder_f,
-					telnum_f:telnum_f,
-					// email_f:email_f,
-					// qq_f:qq_f,
-					descri_f:descri_f
-				},
-				success:function(msg){
-					alert("提交成功");
-					window.location.href=getRootPath()+"/index.php";
-			    }
-			})
-		}
-	})
-
-	$('.things_submit_f').click(function(){
-		editor.sync();
-		var kind_f = $(".kind select[name=kind_f]").val(),
-			name_f = $("input[name=name_f]").val(),
-			locale_f = $("input[name=locale_f]").val(),
-			finder_f = $("input[name=finder_f]").val(),
-			telnum_f = $("input[name=telnum_f]").val(),
-			// email_f = $("input[name=email_f]").val(),
-			// qq_f = $("input[name=qq_f]").val(),
-			descri_f = $("textarea[name=descri_f]").val();
-		var flag = flag_kind&flag_name&flag_locale&flag_finder&flag_telnum;
-		if(flag){
-			$.ajax({
-			type:"POST",
-			url:getRootPath()+"/index.php/descri_find/imformation",
-			data:{
-				kind_f:kind_f,
-				name_f:name_f,
-				locale_f:locale_f,
-				finder_f:finder_f,
-				telnum_f:telnum_f,
-				// email_f:email_f,
-				// qq_f:qq_f,
-				descri_f:descri_f
-			},
-			success:function(msg){
-				alert("提交成功");
-				window.location.href=getRootPath()+"/index.php";
-		    }
-			})
-		}
-	})
+	
 
 	var flag_style = function(flag,str){
-		if(flag){
+		if(flag) {
 			$(str).removeClass("wrong_img").addClass("right_img");
 			console.log("right");
-		}else{
+		} else {
 			$(str).removeClass("right_img").addClass("wrong_img");
 			console.log("wrong");
 		}
@@ -212,7 +249,7 @@ $(document).ready(function(){
 
 
 	// nav_img_show();
-	function nav_img_show(){
+	function nav_img_show() {
 		$('.los_nav ul li').eq(i).css(
 			"background",'#eee url(../../resource/img/tab_bgimg'+(i+1)+'.png) 10% 50% no-repeat'
 		)
@@ -220,86 +257,37 @@ $(document).ready(function(){
 			"background",'#eee url(../../resource/img/tab_bgimg'+(i+1)+'.png) 10% 50% no-repeat'
 		)
 	}
-	var lost_nav = Array('110','140','140','140','140');
-	for(var i=0;i<=4;i++){
+	var lost_nav = ['110','140','140','140','140'];
+	for(var i=0;i<=4;i++) {
 		$('.los_nav ul li').eq(i).css("width",lost_nav[i]+"px");
 		$('.fin_nav ul li').eq(i).css("width",lost_nav[i]+"px");
 	}
-	/*tab bg_img end*/
-
-
-	$('.fin_tab_select').change(function(){
-		 var kind = $(this).val();
-		 var flag = 1;
-		$.ajax({
-			type:"POST",
-			url:getRootPath()+"/index.php/find/page_find_kind",
-			data:{
-				fkind:kind,
-				flag:flag
-			},
-			success:function(msg){
-				// console.log("hah");
-			   $(".fin_detail").html(msg);
-			   $('.fin_detail>ul:even li').css('background',"#ddd");
-			   ul_style();
-			   page_effect();
-		    }
-		})
-	})
-
-
-	$('.los_tab_select').change(function(){
-		 var kind = $(this).val();
-		 var flag = 1;
-		$.ajax({
-			type:"POST",
-			url:getRootPath()+"/index.php/lost/page_lost_kind",
-			data:{
-				lkind:kind,
-				flag:flag
-			},
-			success:function(msg){
-			   $(".los_detail").html(msg);
-			   $('.los_detail>ul:even li').css('background',"#ddd");
-			   ul_style();
-			   page_effect();
-			   // console.log(msg[cur_page]);
-		    }
-		})
-	})
-	function ul_style(){
-		console.log('jaj');
+	
+	function ul_style() {
 		$('.fin_detail>ul:even>li,.los_detail>ul:even>li').css('background','#eee');
-		// $('.los_detail>ul:even>li').css('background','#eee');
-
 	}
 
 
-/*页码样式*/
-page_effect();
-function page_effect(){
-
-    $('.loc_span').css({'color':'#fff','background':'#ef4d4e'});
-}    
-/*end*/
-
+	/*页码样式*/
+	page_effect();
+	function page_effect() {
+		$('.loc_span').css({'color':'#fff','background':'#ef4d4e'});
+	}    
+	/*end*/
 
 
-function loadjs(path)
-{
-	var script = document.createElement("script");
-	script.setAttribute('src',path);
-	document.getElementsByTagName('head')[0].appendChild(script);
-}
+	function loadjs(path) {
+		var script = document.createElement("script");
+		script.setAttribute('src',path);
+		document.getElementsByTagName('head')[0].appendChild(script);
+	}
 
-	function getRootPath() //得到网站的根目录
-  { 
-	var strFullPath=window.document.location.href; 
-	var strPath=window.document.location.pathname; 
-	var pos=strFullPath.indexOf(strPath); 
-	var prePath=strFullPath.substring(0,pos); 
-	var postPath=strPath.substring(0,strPath.substr(1).indexOf('/')+1); 
-	return(prePath+postPath); 
-  } 
+	function getRootPath() { //得到网站的根目录 
+		var strFullPath = window.document.location.href,
+		    strPath = window.document.location.pathname, 
+		    pos = strFullPath.indexOf(strPath),
+		    prePath = strFullPath.substring(0,pos), 
+		    postPath = strPath.substring(0,strPath.substr(1).indexOf('/') + 1);
+		return(prePath + postPath); 
+	} 
 })
